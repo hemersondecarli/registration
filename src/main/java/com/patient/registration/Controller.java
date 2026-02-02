d6eb4aec0b0d4c98b7524216b69460913cc04747
hemersondecarli <hemersondc96@gmail.com>
2026-02-02 08:32:26 +0000

    -Sending a greeting message

---
+++ src/main/java/com/patient/registration/Controller.java
@@ -0,0 +1,15 @@
+package com.patient.registration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String greetings(){
+        return "Welcome to the first page";
+    }
+}
