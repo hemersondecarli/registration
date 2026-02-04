e56f526c20e2e34238cdc7e3b1ac388e972d6e71
Hemerson <hemersondc96@gmail.com>
2026-02-04 09:40:24 +0000

    Moving controller to a package

--- src/main/java/com/patient/registration/Controller/PatientController.java
+++ src/main/java/com/patient/registration/Controller/PatientController.java
@@ -1,4 +1,4 @@
-package com.patient.registration;
+package com.patient.registration.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
