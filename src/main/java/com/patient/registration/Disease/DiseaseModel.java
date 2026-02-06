40b22e267d3d544b06d9ef32f59f10469f937c21
Hemerson <hemersondc96@gmail.com>
2026-02-06 07:57:45 +0000

    Constrcutor / getters&Setters

--- src/main/java/com/patient/registration/Disease/DiseaseModel.java
+++ src/main/java/com/patient/registration/Disease/DiseaseModel.java
@@ -11,5 +11,43 @@ public class DiseaseModel {
 
     }
 
+    public DiseaseModel(Long id, String name, String description, String severity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.severity = severity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
 
 }
