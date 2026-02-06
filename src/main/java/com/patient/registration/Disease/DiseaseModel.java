fbdb6ba24243be1b2245dd8d013294a8d573c0d3
Hemerson <hemersondc96@gmail.com>
2026-02-06 07:55:11 +0000

    Creating disease model

---
+++ src/main/java/com/patient/registration/Disease/DiseaseModel.java
@@ -0,0 +1,15 @@
+package com.patient.registration.Disease;
+
+public class DiseaseModel {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String severity;
+
+    public DiseaseModel(){
+
+    }
+
+
+}
