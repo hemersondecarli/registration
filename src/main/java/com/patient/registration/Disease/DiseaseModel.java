43510b5229edacc6a89c210b81fcf72036798295
Hemerson <hemersondc96@gmail.com>
2026-02-07 22:41:50 +0000

    Adding lombok annotations that takes care of getters/setters, all args and no args constructors

--- src/main/java/com/patient/registration/Disease/DiseaseModel.java
+++ src/main/java/com/patient/registration/Disease/DiseaseModel.java
@@ -2,11 +2,20 @@ package com.patient.registration.Disease;
 
 import com.patient.registration.Patients.PatientModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_disease")
+//Getters Setters
+@Data
+//Constructor no arguments
+@NoArgsConstructor
+//Constructor all arguments
+@AllArgsConstructor
 public class DiseaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,48 +26,5 @@ public class DiseaseModel {
 
     @OneToMany(mappedBy = "disease")
     private List<PatientModel> patient;
-
-    public DiseaseModel(){
-
-    }
-
-    public DiseaseModel(Long id, String name, String description, String severity) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.severity = severity;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getSeverity() {
-        return severity;
-    }
-
-    public void setSeverity(String severity) {
-        this.severity = severity;
-    }
 
 }
