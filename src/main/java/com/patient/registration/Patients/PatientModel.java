e22a6be1423be56468c49ce43fd0b08b03b43969
Hemerson <hemersondc96@gmail.com>
2026-02-07 22:43:08 +0000

    Adding lombok annotations

--- src/main/java/com/patient/registration/Patients/PatientModel.java
+++ src/main/java/com/patient/registration/Patients/PatientModel.java
@@ -2,10 +2,16 @@ package com.patient.registration.Patients;
 
 import com.patient.registration.Disease.DiseaseModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Table(name =  "tb_registration")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PatientModel {
 
     @Id
@@ -20,37 +26,4 @@ public class PatientModel {
     @JoinColumn(name = "disease_id")
     private DiseaseModel disease;
 
-    public PatientModel() {
-
-    }
-
-    public PatientModel(String firstName, String lastName, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
-    
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
